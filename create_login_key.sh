3a52fa9fa796f6407c6f21b76626e60e2e6be2e4
tom <40837110+tommccallum@users.noreply.github.com>
2020-08-26 09:08:55 +0100

    modified script to have standard functions in

--- create_login_key.sh
+++ create_login_key.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+cur_dir=$( cd $(dirname $0) && pwd )
+
+source ${cur_dir}/functions.sh
+
+
 KEY_NAME="my_login_api_key"
 KEY_DESC="key for logging into the ibm cloud cli"
 KEY_FILE="ibm_api_key.txt"
 
-echo "Generating new key in the file ${KEY_FILE}"
+_out "Generating new key in the file ${KEY_FILE}"
 ibmcloud iam api-key-create "${KEY_NAME}" -d "${KEY_DESC}" --file "${KEY_FILE}"
-echo "To check your keys online, find the key named ${KEY_NAME}."
-echo "To find out more, check out https://cloud.ibm.com/docs/account?topic=account-federated_id"
+_out "To check your keys online, find the key named ${KEY_NAME}."
+_out "To find out more, check out https://cloud.ibm.com/docs/account?topic=account-federated_id"
